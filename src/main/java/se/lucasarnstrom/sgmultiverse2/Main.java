a4c27e12d84d0c7281d402d05b5539ccb6f479bc
Lucas Arnström <lucasarnstrom@gmail.com>
2014-04-07 00:22:01 +0200

    Initial upload

---
+++ src/main/java/se/lucasarnstrom/sgmultiverse2/Main.java
@@ -0,0 +1,41 @@
+/**
+ *  Name: Main.java
+ *  Date: 23:32:50 - 5 apr 2014
+ * 
+ *  Author: Lucas Arnström
+ *  
+ *  
+ *  Copyright 2014 Lucas Arnström
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *  
+ *
+ *  Filedescription:
+ *  
+ *  This is the main class of the plugin. This initiates all everything that needs
+ *  to be setup for the plugin to be fully working.
+ * 
+ */
+
+package se.lucasarnstrom.sgmultiverse2;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	
+	public void onEnable() {
+		
+	}
+
+}
