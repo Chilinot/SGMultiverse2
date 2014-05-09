be21a690956e1ac0997b85b16d24701a4397470d
Lucas Arnström <lucas@lucasarnstrom.se>
2014-05-10 00:42:22 +0200

    ...

--- src/main/java/se/lucasarnstrom/sgmultiverse2/misc/IconMenu.java
+++ src/main/java/se/lucasarnstrom/sgmultiverse2/misc/IconMenu.java
@@ -1,30 +1,160 @@
-/**
- *  Author:  Lucas Arnström - LucasEmanuel @ Bukkit forums
- *  Contact: lucasarnstrom(at)gmail(dot)com
- *
- *
- *  Copyright 2014 Lucas Arnström
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program. If not, see <http://www.gnu.org/licenses/>.
- *
- *
- *
- *  Filedescription:
- *
- *
- */
 package se.lucasarnstrom.sgmultiverse2.misc;
 
-public class IconMenu {
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.HandlerList;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.ClickType;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.Plugin;
+
+import java.util.Arrays;
+
+public class IconMenu implements Listener {
+
+	private String name;
+	private int size;
+	private OptionClickEventHandler handler;
+	private Plugin plugin;
+	private Player player;
+
+	private String[] optionNames;
+	private ItemStack[] optionIcons;
+
+	public IconMenu(String name, int size, OptionClickEventHandler handler, Plugin plugin) {
+		this.name = name;
+		this.size = size;
+		this.handler = handler;
+		this.plugin = plugin;
+		this.optionNames = new String[size];
+		this.optionIcons = new ItemStack[size];
+		plugin.getServer().getPluginManager().registerEvents(this, plugin);
+	}
+
+	public IconMenu setOption(int position, ItemStack icon, String name, String... info) {
+		optionNames[position] = name;
+		optionIcons[position] = setItemNameAndLore(icon, name, info);
+		return this;
+	}
+
+	public void setSpecificTo(Player player) {
+		this.player = player;
+	}
+
+	public boolean isSpecific() {
+		return player != null;
+	}
+
+	public void open(Player player) {
+		Inventory inventory = Bukkit.createInventory(player, size, name);
+		for(int i = 0; i < optionIcons.length; i++) {
+			if(optionIcons[i] != null) {
+				inventory.setItem(i, optionIcons[i]);
+			}
+		}
+		player.openInventory(inventory);
+	}
+
+	public void destroy() {
+		HandlerList.unregisterAll(this);
+		handler = null;
+		plugin = null;
+		optionNames = null;
+		optionIcons = null;
+	}
+
+	@EventHandler(priority = EventPriority.HIGHEST)
+	void onInventoryClick(InventoryClickEvent event) {
+		if(event.getInventory().getTitle().equals(name) && (player == null || event.getWhoClicked() == player)) {
+			event.setCancelled(true);
+			if(event.getClick() != ClickType.LEFT) {
+				return;
+			}
+			int slot = event.getRawSlot();
+			if(slot >= 0 && slot < size && optionNames[slot] != null) {
+				Plugin plugin = this.plugin;
+				OptionClickEvent e = new OptionClickEvent((Player) event.getWhoClicked(), slot, optionNames[slot], optionIcons[slot]);
+				handler.onOptionClick(e);
+				((Player) event.getWhoClicked()).updateInventory();
+				if(e.willClose()) {
+					final Player p = (Player) event.getWhoClicked();
+					Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
+						public void run() {
+							p.closeInventory();
+						}
+					});
+				}
+				if(e.willDestroy()) {
+					destroy();
+				}
+			}
+		}
+	}
+
+	public interface OptionClickEventHandler {
+		public void onOptionClick(OptionClickEvent event);
+	}
+
+	public class OptionClickEvent {
+		private Player player;
+		private int position;
+		private String name;
+		private boolean close;
+		private boolean destroy;
+		private ItemStack item;
+
+		public OptionClickEvent(Player player, int position, String name, ItemStack item) {
+			this.player = player;
+			this.position = position;
+			this.name = name;
+			this.close = true;
+			this.destroy = false;
+			this.item = item;
+		}
+
+		public Player getPlayer() {
+			return player;
+		}
+
+		public int getPosition() {
+			return position;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public boolean willClose() {
+			return close;
+		}
+
+		public boolean willDestroy() {
+			return destroy;
+		}
+
+		public void setWillClose(boolean close) {
+			this.close = close;
+		}
+
+		public void setWillDestroy(boolean destroy) {
+			this.destroy = destroy;
+		}
+
+		public ItemStack getItem() {
+			return item;
+		}
+	}
+
+	private ItemStack setItemNameAndLore(ItemStack item, String name, String[] lore) {
+		ItemMeta im = item.getItemMeta();
+		im.setDisplayName(name);
+		im.setLore(Arrays.asList(lore));
+		item.setItemMeta(im);
+		return item;
+	}
 }
